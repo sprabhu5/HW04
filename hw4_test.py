f54cd0587a9aa7aa775cb3414a522b0242b42c38
sprabhu5 <58062934+sprabhu5@users.noreply.github.com>
2020-02-19 00:16:41 -0500

    Create hw4_test.py

---
+++ hw4_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from githubapi import GithubApi
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestGithubAPI(unittest.TestCase):
+    def testGithub(self):
+        self.assertEqual(GithubApi('?'), False)
+    def testGithub2(self):
+        self.assertEqual(GithubApi('jjjpanda'), True)
+       
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
