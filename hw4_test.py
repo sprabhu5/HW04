a53b47d145f9ebe01cf3327a511308db42bf5e47
sprabhu5 <58062934+sprabhu5@users.noreply.github.com>
2020-03-07 18:39:31 -0500

    Update hw4_test.py

--- hw4_test.py
+++ hw4_test.py
@@ -4,9 +4,6 @@ import unittest.mock as mock
 
 from hw4 import GithubApi
 
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
-
 class TestGithubAPI(unittest.TestCase):
     def testGithub(self):
         with mock.patch('hw4.GithubAPI', create=True) as MockGithub:
@@ -15,8 +12,7 @@ class TestGithubAPI(unittest.TestCase):
     def testGithub2(self):
         with mock.patch('hw4.GithubAPI', create=True) as MockGithub:
             MockGithub.return_value = True
-            self.assertEqual(MockGithub('jjjpanda'), True)
+            self.assertEqual(MockGithub('sprabhu5'), True)
        
 if __name__ == '__main__':
-    print('Running unit tests')
     unittest.main()
