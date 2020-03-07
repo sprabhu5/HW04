aa4931e9a5d9fca16dc27764ba9385bbcbc71053
sprabhu5 <58062934+sprabhu5@users.noreply.github.com>
2020-03-07 18:25:20 -0500

    Update hw4_test.py

--- hw4_test.py
+++ hw4_test.py
@@ -9,11 +9,11 @@ from hw4 import GithubApi
 
 class TestGithubAPI(unittest.TestCase):
     def testGithub(self):
-        with mock.patch('githubapi.GithubAPI', create=True) as MockGithub:
+        with mock.patch('hw4.GithubAPI', create=True) as MockGithub:
             MockGithub.return_value = False
             self.assertEqual(MockGithub('?'), False)
     def testGithub2(self):
-        with mock.patch('githubapi.GithubAPI', create=True) as MockGithub:
+        with mock.patch('hw4.GithubAPI', create=True) as MockGithub:
             MockGithub.return_value = True
             self.assertEqual(MockGithub('jjjpanda'), True)
        
