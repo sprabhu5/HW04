d843a8bc79626b86236d9a57f33a8947f85571c0
sprabhu5 <58062934+sprabhu5@users.noreply.github.com>
2020-03-07 14:57:41 -0500

    Update hw4_test.py

--- hw4_test.py
+++ hw4_test.py
@@ -1,13 +1,24 @@
+"""
+@author:Shivani
+HW04
+"""
 import unittest
+from unittest import mock
+from HW04 import repositoryList
 
-from hw4 import GithubApi
+
+@mock.patch('urllib.request.urlopen')
+class TestRepo(unittest.TestCase):
+    """Test for the repositoryList function"""
+
+    def test_commits(self, other):
+        repo = [value for value in repositoryList("Shivani-Prabhu")]
+        result = ["Repo: GitHubApi567, commits: 12", "Repo: hw4, commits: 1", "Repo: StaticCodeAnalysis567, "
+                                                                                   "commits: 2", "Repo: Triangle567, "
+                                                                                                 "commits: 12"]
+        self.assertEqual(repo, result)
 
 
-class TestGithubAPI(unittest.TestCase):
-    def testGithub(self):
-        self.assertEqual(GithubApi('?'), False)
-    def testGithub2(self):
-        self.assertEqual(GithubApi('sprabhu5'), True)
-       
-if __name__ == '__main__':
-    unittest.main()
+
+if _name_ == '_main_':
+    unittest.main(exit=False, verbosity=2)
